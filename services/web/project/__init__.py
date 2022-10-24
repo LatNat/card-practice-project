a589efe59e12180341e1352d3de7ccb6b6c7864d
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-10-24 12:49:16 +0200

    Add: basic tables for database

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -7,21 +7,27 @@ app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
 
 
+class Type(db.Model):
+    __tablename__ = "deck_type"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(128), unique=True, nullable=False)
+    
+
 class Suit(db.Model):
-    __tablename__ = "suits"
+    __tablename__ = "suit"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
-
-    def __init__(self, name):
-        self.name = name
+    deck_type_id = db.Column(db.Integer, db.ForeignKey("deck_type.id"))
 
 
 class Card(db.Model):
     __tablename__ = "cards"
 
     id = db.Column(db.Integer, primary_key=True)
-    suit = db.Column(db.Integer, ForeignKey("suits.id")
+    type = db.Column(db.Integer, db.ForeignKey("deck_type.id"))
+    suit = db.Column(db.Integer, db.ForeignKey("suit.id"))
     value = db.Column(db.Integer)
 
 
--- services/web/project/config.py
+++ services/web/project/config.py
@@ -1,7 +1,7 @@
 import os
 
 
-basedir = os.path.abspath(os.path.diranem(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):

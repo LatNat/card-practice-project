05d450b3f92a94ba9bc777d636ecbaaf97d88f2d
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-10-25 13:45:41 +0200

    Refactor: card creation in manage.py seed_db function

--- services/web/manage.py
+++ services/web/manage.py
@@ -15,19 +15,40 @@ def create_db():
 
 @cli.command("seed_db")
 def seed_db():
-	suit_names = ["Diamonds", "Hearts", "Spades", "Clubs"]
+	standard_deck=Type(name="French")
 	
-	standard_deck_type=Type(name="French")
+	create_all_suits_for_deck(standard_deck)
+	create_all_card_values_for_deck(standard_deck)	
 	
-	for sn in suit_names:
-		current_suit = Suit(name=sn)
-		standard_deck_type.used_suits.append(current_suit)
-		db.session.add(current_suit)
-		
-	
-	db.session.add(standard_deck_type)
+	db.session.add(standard_deck)
 	db.session.commit()
 
 
+def create_all_suits_for_deck(deck):
+	suit_names = ["Diamonds", "Hearts", "Spades", "Clubs"]
+	
+	for sn in suit_names:
+		current_suit = Suit(name=sn)
+		deck.used_suits.append(current_suit)
+		db.session.add(current_suit)
+
+
+def create_all_card_values_for_deck(deck):
+	# creates all the values(13 different) needed for the standard 52 card french deck
+	for card in range(1,14):
+		if card == 1:
+			card_value = Value(value=11, name="Ace")
+		elif card < 11:
+			card_value = Value(value=card, name=str(card))
+		elif card == 11:
+			card_value = Value(value=10, name="Jack")
+		elif card == 12:
+			card_value = Value(value=10, name="Queen")
+		elif card == 13:
+			card_value = Value(value=10, name="King")
+		deck.used_values.append(card_value)
+		db.session.add(card_value)
+
+	
 if __name__ == "__main__":
     cli()

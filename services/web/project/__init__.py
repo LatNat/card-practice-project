0327dc002a5005579f18154623a6559f30cd43c3
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-10-26 15:40:11 +0200

    Refactor: all .py files to conform to flake8 linter rules

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -8,30 +8,28 @@ db = SQLAlchemy(app)
 
 
 value_associations = db.Table(
-   "value_associations",
-   db.Model.metadata,
-   db.Column("deck_type_id", db.ForeignKey("deck_types.id"), primary_key=True),
-   db.Column("value_id", db.ForeignKey("values.id"), primary_key=True),
+    "value_associations",
+    db.Model.metadata,
+    db.Column("deck_type_id", db.ForeignKey("deck_types.id"), primary_key=True),
+    db.Column("value_id", db.ForeignKey("values.id"), primary_key=True),
 )
 
 
 suit_associations = db.Table(
-   "suit_associations",
-   db.Model.metadata,
-   db.Column("deck_type_id", db.ForeignKey("deck_types.id"), primary_key=True),
-   db.Column("suit_id", db.ForeignKey("suits.id"), primary_key=True),
+    "suit_associations",
+    db.Model.metadata,
+    db.Column("deck_type_id", db.ForeignKey("deck_types.id"), primary_key=True),
+    db.Column("suit_id", db.ForeignKey("suits.id"), primary_key=True),
 )
 
 
 class Type(db.Model):
     __tablename__ = "deck_types"
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
-    used_suits = db.relationship(
-    	"Suit", secondary=suit_associations, back_populates="deck_type")
-    used_values = db.relationship(
-    	"Value", secondary=value_associations, back_populates="deck_type")
+    used_suits = db.relationship("Suit", secondary=suit_associations, back_populates="deck_type")
+    used_values = db.relationship("Value", secondary=value_associations, back_populates="deck_type")
 
 
 class Suit(db.Model):
@@ -39,8 +37,7 @@ class Suit(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
-    deck_type = db.relationship(
-    	"Type", secondary=suit_associations, back_populates="used_suits")
+    deck_type = db.relationship("Type", secondary=suit_associations, back_populates="used_suits")
 
 
 class Value(db.Model):
@@ -49,8 +46,7 @@ class Value(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(128), nullable=False)
-    deck_type = db.relationship(
-    	"Type", secondary=value_associations, back_populates="used_values")
+    deck_type = db.relationship("Type", secondary=value_associations, back_populates="used_values")
 
 
 @app.route("/")
